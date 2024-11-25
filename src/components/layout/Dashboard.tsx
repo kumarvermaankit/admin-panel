e8ef3941b9513b15c58d5c70f8fda3e29d40c69a
Ankit Kumar Verma <kumarvermaankit9555@gmail.com>
2024-11-25 20:38:48 +0530

    Added note to dashboard

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -13,11 +13,23 @@ export const Dashboard = () => {
     { title: "Spin Wheel - Quiz Answered", value: quizCount , key: "3" },
     // { title: "Bus Arrivals", value: busArrivalCount , key: "4" },
     { title: "Feedback Submissions", value: feedbackCount , key: "5" },
-
   ];
 
   return (
     <div style={{ padding: "20px" }}>
+      <div
+        style={{
+          color: "red",
+          fontSize: "18px",
+          fontWeight: "bold",
+          marginBottom: "20px",
+          textAlign: "left",
+        }}
+      >
+        Note: The data provided here represents the total count of records and
+        is not the final data. The final data will be aggregated by Staff ID,
+        location, and event date.
+      </div>
       <Row gutter={[16, 16]}>
         {cardData.map((card) => (
           <Col key={card.key} xs={24} sm={12} md={8} lg={6}>
